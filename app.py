0d05c88de478fd19c9cba1610d51c81baba64bd5
stefanovic80 <stefanovicgh@gmail.com>
2025-06-13 20:25:18 +0200

    requirements.txt added as it is needed for render!

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def function():
+def home():
     return "here we go!"
+
+@app.route("/anotherPage/")
+def anotherPage():
+    return "here another page!"
 
 if __name__ == "__main__":
     app.run(port = 5000)
