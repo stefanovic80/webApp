427e3fe453b4e784c2460c7492d5a4509b1a1eca
stefanovic80 <stefanovicgh@gmail.com>
2025-06-13 22:04:04 +0200

    other updates!

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def home():
     return "cazzarola!"
 
+@app.route("/stracazzarola!")
+def home2():
+    return "aristracazzaraccappottabilmente!")
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
