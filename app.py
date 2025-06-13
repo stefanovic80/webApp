5f0701b75dd26c4bab5162897995bc178c685dc9
stefanovic80 <stefanovicgh@gmail.com>
2025-06-13 21:54:28 +0200

    again updates!

--- app.py
+++ app.py
@@ -1,19 +1,13 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "here we go!"
-
-@app.route("/anotherPage/")
-def anotherPage():
-    return "here another page!"
-
+    return "cazzarola!"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
 
-#if __name__ == "__main__":
-#    app.run(port = 5000)
