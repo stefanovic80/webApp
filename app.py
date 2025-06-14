2621977167d5b29fcc2d322370115169e545a1e1
stefanovic80 <stefanovicgh@gmail.com>
2025-06-14 09:36:15 +0200

    html code added!

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "cazzarola!"
+    return render_template('index.html')
 
 @app.route("/stracazzarola/")
 def home2():
     return "aristracazzaraccappottabilmente!"
 
 if __name__ == "__main__":
+    #app.run(port = 5000)
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
-
